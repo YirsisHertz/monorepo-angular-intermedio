5d97339c49402eaf5f9bbcaa7b1d2321147d0598
Yirsis Serrano <yirsishertz02@gmail.com>
2023-01-31 01:18:34 -0600

    Storybook

--- libs/shared/ui/src/lib/button/button.component.ts
+++ libs/shared/ui/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'ainter-button',
@@ -6,11 +6,35 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+  /**
+   * Define si el color es primario (yellow)
+   */
   @Input()
   isPrimary: boolean = false;
 
+  /**
+   * Define si el boton esta redondeado (1 rem)
+   */
   @Input()
   isRounded: boolean = false;
+
+  /**
+   * Define un texto personalizado
+   */
+  @Input()
+  text: string = 'Default text';
+
+  /**
+   * Custom Color
+   */
+  @Input()
+  customColor?: string;
+
+  /**
+   * Custom Foreground Text
+   */
+  @Input()
+  customForeground?: string;
 
   get roundedClass() {
     return this.isRounded ? 'rounded' : '';
